2ddba0857f21e3d8e1602c1165de099f03a78ef3
Alex J <alexnj@yahoo-inc.com>
2014-07-08 19:29:15 -0700

    Browser detection enabled.

--- web.js
+++ web.js
@@ -1,15 +1,19 @@
-// web.js
+
 var express = require("express");
 var logfmt = require("logfmt");
+var socialbrowser = require("social-browser");
 var app = express();
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send(req.headers);
+    var browser = socialbrowser.detect(req);
+    if (browser) {
+        res.send("<h1>You are using " + browser.name + "</h1>");
+    }
 });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
